33300a3c9270d64f630e2cb0aae1be1527d7f37b
Mennatalla-Khougha <menmen.khougha@gmail.com>
2024-01-23 12:31:12 +0200

    tasks 9

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -1,1 +1,20 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+Define Python function that inserts a new document in a collection based
+on kwargs
+"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """
+    The function `insert_school` inserts a document into a MongoDB
+    collection with the provided keyword arguments and returns the
+    inserted document's ID.
+
+    :param mongo_collection: The `mongo_collection` parameter is the
+    collection in the MongoDB database where you want to insert the
+    school document
+    :return: the inserted ID of the document in the MongoDB collection.
+    """
+    result = mongo_collection.insert_one(kwargs)
+    return result.inserted_id
