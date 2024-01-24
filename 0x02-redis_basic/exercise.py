df707df73e00275f51573074b42eab5ed904e6b0
Mennatalla-Khougha <menmen.khougha@gmail.com>
2024-01-24 14:13:52 +0200

    task 0

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 """Create a Cache class"""
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Any
 
 
 class Cache:
@@ -26,3 +26,46 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Callable = None) -> Any:
+        """_summary_
+
+        Args:
+            key (str): the randomly generated key
+            fn (Callable, optional):  will be used to convert the data back
+            to the desired format. Defaults to None.
+
+        Returns:
+            Any: the desired format
+        """
+        result = self._redis.get(key)
+
+        if result is None:
+            return None
+
+        if fn is not None:
+            return fn(result)
+
+        return result
+
+    def get_str(self, key: str) -> str:
+        """get_str that will automatically parametrize Cache.get with str
+
+        Args:
+            key (str): the randomly generated key
+
+        Returns:
+            str: the type of the cache
+        """
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> int:
+        """get_int that will automatically parametrize Cache.get with int
+
+        Args:
+            key (str): the randomly generated key
+
+        Returns:
+            int: the type of the cache
+        """
+        return self.get(key, fn=int)
