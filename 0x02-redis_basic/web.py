358406d2900fda22043c723efaabe96add5be80a
Mennatalla-Khougha <menmen.khougha@gmail.com>
2024-01-24 23:40:43 +0200

    task 5

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -4,33 +4,44 @@ import redis
 import requests
 from functools import wraps
 
-red = redis.Redis()
+r = redis.Redis()
+count = 0
 
 
-def url_count(method):
-    """Decorator for get_page function"""
-    @wraps(method)
-    def wrapper(url):
-        """Wrapper function"""
-        red.incr(f"count:{url}")
-        value = red.get(f"cached:{url}")
-        if value:
-            return value.decode("utf-8")
-
-        html = method(url)
-        red.setex(f"cached:{url}", 10, html)
-        # red.set(f"cached:{url}", html)
-        # red.expire(f"cached:{url}", 10)
-        # red.setex(f"cached:{url}", 10, html)
-        return html
-    return wrapper
-
-
-@url_count
 def get_page(url: str) -> str:
-    """obtain the HTML content"""
-    results = requests.get(url)
-    return results.text
+    """ track how many times a particular URL was accessed in the key
+        "count:{url}"
+        and cache the result with an expiration time of 10 seconds """
+    r.set(f"cached:{url}", count)
+    resp = requests.get(url)
+    r.incr(f"count:{url}")
+    r.setex(f"cached:{url}", 10, r.get(f"cached:{url}"))
+    return resp.text
+
+# def url_count(method):
+#     """Decorator for get_page function"""
+#     @wraps(method)
+#     def wrapper(url):
+#         """Wrapper function"""
+#         red.incr(f"count:{url}")
+#         value = red.get(f"cached:{url}")
+#         if value:
+#             return value.decode("utf-8")
+
+#         html = method(url)
+#         red.setex(f"cached:{url}", 10, html)
+#         # red.set(f"cached:{url}", html)
+#         # red.expire(f"cached:{url}", 10)
+#         # red.setex(f"cached:{url}", 10, html)
+#         return html
+#     return wrapper
+
+
+# @url_count
+# def get_page(url: str) -> str:
+#     """obtain the HTML content"""
+#     results = requests.get(url)
+#     return results.text
 
 
 if __name__ == "__main__":
