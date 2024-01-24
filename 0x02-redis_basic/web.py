e92885793ed0af3c0bb7c7092cd45a05736113e6
Mennatalla-Khougha <menmen.khougha@gmail.com>
2024-01-24 23:18:58 +0200

    task 5

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,32 +1,25 @@
 #!/usr/bin/env python3
-""" implement a get_page function"""
-import requests
+"""Implement a get_page function."""
 import redis
+import requests
 from functools import wraps
-from typing import Callable
-
 
 red = redis.Redis()
 
 
-def url_count(method: Callable) -> Callable:
-    """
-    Decorator that takes a single method Callable argument and returns
-    a Callable
-    """
-
+def url_count(method):
+    """decorator for get_page function"""
     @wraps(method)
     def wrapper(url):
-        """
-        Wrapper function to  increments the count for that key every time
-        the method is called
-        """
-        key = "cached" + url
+        """wrapper function"""
+        key = "cached:" + url
         value = red.get(key)
         if value:
             return value.decode("utf-8")
-        count = "count" + url
+
+        count = "count:" + url
         html = method(url)
+
         red.incr(count)
         red.set(key, html, ex=10)
         red.expire(key, 10)
@@ -36,12 +29,9 @@ def url_count(method: Callable) -> Callable:
 
 @url_count
 def get_page(url: str) -> str:
-    """
-    It uses the requests module to obtain the HTML content of
-    a particular URL and returns i
-    """
-    response = requests.get(url)
-    return response.text
+    """obtain the HTML content"""
+    results = requests.get(url)
+    return results.text
 
 
 if __name__ == "__main__":
